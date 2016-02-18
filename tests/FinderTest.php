9fc72810b924b46d85a0d81a8907a881f1ea5116
Josh Overly <joshoverly@yahoo.com>
2016-02-18 11:32:04 -0800

    initial commit, pass first spec

--- tests/FinderTest.php
+++ tests/FinderTest.php
@@ -2,7 +2,7 @@
     require_once "src/Finder.php";
     class FinderTest extends PHPUnit_Framework_TestCase
     {
-        function test_makeFinder_changeMonth()
+        function test_makeFinder_changeMonthOne()
         {
             //Arrange
             $test_Score = new Finder(7,4,1776);
@@ -13,6 +13,18 @@
             $result = $test_Score->setMonth(7);
             //Assert tells our tests what to expect from the output of our method.
             $this->assertEquals(5, $result);
+        }
+        function test_makeFinder_changeMonthTwo()
+        {
+            //Arrange
+            $test_Score = new Finder(7,4,1776);
+            // $input_month = 7;
+            // $input_day = 4;
+            // $input_year = 1776;
+            //Act: runs the actual method that we are testing.
+            $result = $test_Score->setMonth(1);
+            //Assert tells our tests what to expect from the output of our method.
+            $this->assertEquals(11, $result);
         }
 
 
